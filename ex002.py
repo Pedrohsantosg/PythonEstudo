bd5d6e245d3e956a5b555524aefb8f604f450c62
Pedrohsantosg <pedro.henrique.gomes@maisunifacisa.com.br>
2024-02-27 15:26:09 -0300

    resolution of 001-005

---
+++ ex002.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+equipe = input('Qual a cor da sua equipe? ')
+
+print('É um prazer te conhecer {}, Bem vindo!'.format(nome))
+print('Sua equipe escolhida foi a {}'.format(equipe))
